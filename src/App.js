8b04f34db8a38c25beb856980ee42a17fce36228
AlexandreCampos <alexandrerazrmaxx@gmail.com>
2021-11-28 13:35:03 -0400

    utilizando o axios para acessar os dados da api e retornando para a pagina

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import api from './api';
 
 class App extends Component {
 
-  async componentDidMount() {
-    const response = await api.get('/1')
+  state = {
+    artigos:[],
+  }
 
-    console.log(response.data);
+  async componentDidMount() {
+    const response = await api.get('/')
+
+    //console.log(response.data);
+    this.setState({artigos: response.data})
   }
   render(){
+
+    const {artigos} = this.state;
+    console.log(artigos);
+
     return(
       <div>
         <h1>Artigos Listados </h1>
+        <ul>
+          {artigos.map(artigo =>(
+            <li key={artigo._id}>
+            <h2>nome: {artigo.nome}</h2>
+          </li>
+          ) 
+          )}
+        </ul>
       </div>
     );
   }
